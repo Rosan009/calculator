cb64830410fa43859f55d1298655d8da58afc4ba
Rosan009 <rosannarwal4@gmail.com>
2024-03-29 13:23:11 +0530

    commit

---
+++ javacript.js
@@ -0,0 +1,20 @@
+function display(value){
+    document.getElementById("result").value+=value;
+}
+ function clearResult() {
+      document.getElementById("result").value = "";
+    }
+function clearResult1() {
+      var result = document.getElementById("result").value;
+        document.getElementById("result").value = result.slice(0, -1);
+      }    
+function calculate() {
+     var result = document.getElementById("result").value;
+    try {
+      var finalResult = eval(result);
+    document.getElementById("result").value = finalResult;
+      }
+       catch (error) {
+        alert("Invalid expression!");
+     }
+    }
